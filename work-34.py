5ff2664fd8d104125915b92d11948a55000842d8
ElbekYusupov-1 <elbekbusnes@gmail.com>
2026-04-15 11:43:12 +0500

    Add files via upload

---
+++ work-34.py
@@ -0,0 +1,25 @@
+#1-misol
+#def juft(n):
+ #   if n % 2 == 0:
+  #      return "Juft"
+   # else:
+    #    return "Toq"
+#
+#print(juft(4575))
+ 
+#2-misol
+#def son(n):
+ #   return n**2,n**3
+    
+#print(son(8))
+
+#3-misol
+#def son(n):
+ #   if n==0:
+  #      print("bu son 0 ")
+   # elif n<0:
+    #    print("manfiy")
+     #   
+    #else:
+    #    print("musbat")
+#son(-2)
